654c68da2a14e5599aacfde6e2af6f457b0d7d0f
Agent200147 <91262640+Agent200147@users.noreply.github.com>
2022-05-11 02:03:00 +0300

    api_final

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -235,6 +235,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getApiToken(): ?string
+    {
+        return $this->apiToken;
+    }
+
+    public function setApiToken(string $apiToken): self
+    {
+        $this->apiToken = $apiToken;
+
+        return $this;
+    }
+    
     public function __toString(){
         return $this->email;
     }
